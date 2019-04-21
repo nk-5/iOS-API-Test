cf4f2a808ad4ee62333515fae589c5b3b24b017b
nkws <keigo_nakagawa@voyagegroup.com>
2019-04-21 21:27:58 +0900

    RsSwift project update swift version

--- RxSwift/RxSwiftLearn/ViewController.swift
+++ RxSwift/RxSwiftLearn/ViewController.swift
@@ -12,22 +12,48 @@ import RxCocoa
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var text: UITextField!
-    @IBOutlet weak var label: UILabel!
-    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var testInputField: UITextField!
+    @IBOutlet weak var testResultLabel: UILabel!
+    @IBOutlet weak var testActionButton: UIButton!
     
     let bag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-//        text.rx.controlEvent()
-        button.rx.tap.subscribe { observer in
+   
+
+        let inputText = testInputField.rx.controlEvent(.editingDidEndOnExit).asObservable()
+            .map { self.testInputField.text }
+            .filter { ($0 ?? "").count > 0 }
+            .asDriver(onErrorJustReturn: "error")
+        
+
+        inputText
+            .drive(testResultLabel.rx.text)
+            .disposed(by: bag)
+
+        let hasText = testInputField.rx.text
+            .map { ($0 ?? "").count > 0 }
+            .asDriver(onErrorJustReturn: false)
+
+        hasText
+            .drive(testActionButton.rx.isEnabled)
+            .disposed(by: bag)
+
+        testActionButton.rx.tap.subscribe { observer in
             print(observer)
             print("button tapped")
         }
         .disposed(by: bag)
+        
     }
+    
+    private func error() throws {
+       throw Errors.errorTest
+    }
+}
 
+enum Errors: Error {
+    case errorTest
 }
 
