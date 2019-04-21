a4a9795f94ea4b9e6a2c1e5c04951186558e4265
nkws <nkws27@gmail.com>
2019-04-22 00:04:31 +0900

    detect plane surface

--- ARKit/FirstAR/FirstAR/ViewController.swift
+++ ARKit/FirstAR/FirstAR/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import ARKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSessionDelegate {
 
     @IBOutlet weak var sceneView: ARSCNView!
     
@@ -17,10 +17,24 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         sceneView.scene = SCNScene(named: "art.scnassets/ship.scn")!
+        sceneView.session.delegate = self
         
         let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
         
         sceneView.session.run(configuration)
+    }
+    
+    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+        print("add plane")
+    }
+    
+    func session(_ session: ARSession, didRemove anchors: [ARAnchor]) {
+        print("remove plane")
+    }
+    
+    func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
+        print("update plane")
     }
 
 
